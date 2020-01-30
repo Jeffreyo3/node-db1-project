f5d58010997da6a0fb9d6d8ef7a5e22d774c12be
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-01-29 19:30:29 -0800

    Add CRUD to /api/accounts/ route
    Update README.md

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -3,19 +3,61 @@ const db = require('../data/dbConfig');
 const router = express.Router();
 
 // GET
-router.get('/', async (req,res)=> {
+router.get('/', async (req, res) => {
     try {
         const accounts = await db('accounts')
         res.json(accounts)
     } catch (err) {
-        res.status(500).json({message: "Failed to get accounts", err})
+        res.status(500).json({ message: "Failed to get accounts", err });
     }
 });
 
 // GET by id
+router.get('/:id', async (req, res) => {
+    try {
+        const [account] = await db('accounts').where('id', req.params.id);
+        res.json(account);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to get account", err });
+    }
+});
+
 // CREATE
+router.post('/', async (req, res) => {
+    const accountData = req.body;
+    try {
+        const account = await db('accounts').insert(accountData);
+        res.status(201).json(account);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to add account", err });
+    }
+})
+
 // UPDATE by id
-// DELETE
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const rowsUpdated = await db('accounts')
+            .where('id', id)
+            .update(req.body);
+        res.status(200).json({ updated: rowsUpdated });
+    } catch (err) {
+        res.status(500).json({ message: "Failed to update account", err });
+    }
+});
+
+// DELETE by id
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const rowsDeleted = await db('accounts')
+            .where('id', id)
+            .del();
+        res.status(200).json({ deletedRecords: rowsDeleted })
+    } catch (err) {
+        res.status(500).json({ message: "Failed to delete account", err });
+    }
+});
 
 
 module.exports = router;
